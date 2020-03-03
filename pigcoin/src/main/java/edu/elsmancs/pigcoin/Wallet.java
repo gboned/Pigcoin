1d55bcda1a76bd9f9b1232d99da5fa9d8f1cdd95
gboned <gboned@cifpfbmoll.eu>
2020-03-03 02:47:01 +0100

    Añadidos atributos de clase wallet

---
+++ pigcoin/src/main/java/edu/elsmancs/pigcoin/Wallet.java
@@ -0,0 +1,18 @@
+package edu.elsmancs.pigcoin;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import edu.elsmancs.pigcoin.GenSig;
+
+public class Wallet {
+	
+	PublicKey adress = null;
+	PrivateKey sKey = null;
+	int total_input = 0;
+	int total_output = 0;
+	int balance = 0;
+	int inputTransactions = 0;
+	int outputTransactions = 0;
+
+}
+
