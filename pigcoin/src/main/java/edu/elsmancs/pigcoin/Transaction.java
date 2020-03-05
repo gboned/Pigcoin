14d0775b4fd22d91fbcd8dfb72699f7bceb86215
gboned <gboned@cifpfbmoll.eu>
2020-03-05 15:31:12 +0100

    refactor(Transaction): Cambiar posición atributos

--- pigcoin/src/main/java/edu/elsmancs/pigcoin/Transaction.java
+++ pigcoin/src/main/java/edu/elsmancs/pigcoin/Transaction.java
@@ -11,6 +11,10 @@ public class Transaction {
 	private double pigcoins = 0;
 	private String message = null;
 	
+	public Transaction() {	
+		
+	}	
+	
 	public Transaction(String hash, String prev_hash, PublicKey pKey_sender, PublicKey pKey_recipient, double pigcoins, String message) {
 		this.hash = hash;
 		this.prev_hash = prev_hash;
